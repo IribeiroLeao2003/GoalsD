5a906c0a3ea6ccdae97477bb40ff3f8c2e1b5dc8
IribeiroLeao2003 <96347353+IribeiroLeao2003@users.noreply.github.com>
2023-04-04 14:13:00 -0400

    App in Beta Version

---
+++ DailyGoalsapp/DailyGoalsapp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DailyGoalsapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialize the timer and set the interval to 1 second
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            // Subscribe to the Tick event to update the clock
+            timer.Tick += Timer_Tick;
+
+            // Start the timer
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Get the current date and time
+            DateTime currentTime = DateTime.Now;
+
+            // Update the text of the clock label
+            CurrentTime.Text = currentTime.ToString("hh:mm:ss tt");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService navService = NavigationService.GetNavigationService(this);
+            navService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            this.Close();
+        }
+    }
+}
